fadc076b7305d47dbc2409b00c4fe64f8630e683
Carlos V <cavarod@gmail.com>
2021-02-11 16:25:52 -0800

    added the money function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,13 +1,14 @@
 // The first part of the game
 
-//Ask the player for his robot name
+//Assigns the player's robot a name
 var robotName = window.prompt("Welcome Neophyte. It's time to name your robot!");
 
-//Give the player health and attack
-var playerHealth = 100;
-var playerAttack = 10;
+//Give the player health, attack, money
+var robotHealth = 100;
+var robotAttack = 10;
+var robotMoney = 10;
 
-console.log(robotName, playerHealth, playerAttack);
+console.log(robotName, robotHealth, robotAttack);
 
 // Enemy Attributes
 var enemyName = "Roborto";
@@ -18,33 +19,53 @@ function fight () {
 	//Welcome Message
 	window.alert("Let's get ready to rumble!");
 
-	//Subtract the value of playerAttack from enemyHealth
-	enemyHealth = enemyHealth - playerAttack;
+	var promptFight = window.prompt("Would you like to Fight or Skip this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
-	//Log a message with the result
-	console.log(robotName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
-	//Subtract the vale of enemyAttack from playerHealth
-	playerHealth = playerHealth - enemyAttack;
+	//if player chooses to fight, then fight
+	if (promptFight === 'fight' ||promptFight === "FIGHT") {
 
-	//Log a message with the result
-	console.log(enemyName + " attacked " + robotName + ". " + robotName + " now has " + playerHealth + " health remaining.");
+		//Subtract the value of playerAttack from enemyHealth
+		enemyHealth = enemyHealth - robotAttack;
 
-	//Chek Players Health
-	if (playerHealth <=0) {
-		window.alert(robotName + " has died!");
+		//Log a message with the result
+		console.log(robotName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+		//Subtract the vale of enemyAttack from robotHealth
+		playerHealth = robotHealth - enemyAttack;
+
+		//Log a message with the result
+		console.log(enemyName + " attacked " + robotName + ". " + robotName + " now has " + robotHealth + " health remaining.");
+
+		//Chek Players Health
+		if (robotHealth <=0) {
+			window.alert(robotName + " has died!");
+		}
+		else {
+			window.alert(robotName + " still has " + robotHealth + " health left.");
+		};
+
+		//Check Enemy's Health
+		if (enemyHealth <= 0 ) {
+			window.alert(enemyName + " has died!");
+		}
+		else {
+			window.alert(enemyName + " still has " + enemyHealth + " health left.");
+		};
+	} else if (promptFight === "skip" || promptFight === "SKIP") {
+			//Player confirms they want to skip
+			var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+			//leave the fighght if yest
+			if (confirmSkip) {
+				window.alert(robotName + " has chosen to skip the fight!");
+				//subtract money from playerMoney for skipping
+				playerMoney = playerMoney - 2;
+			} else {
+				fight();
+			};
+
+	} else {
+		window.alert("You need to choose a valid option. Try again!");
 	}
-	else {
-		window.alert(robotName + " still has " + playerHealth + "health left.");
-	};
-
-	//Check Enemy's Health
-	if (enemyHealth <= 0 ) {
-		window.alert(enemyName + " has died!");
-	}
-	else {
-		window.alert(enemyName + " still has " + enemyHealth + "health left.");
-	};
-
 };
 
 fight();
